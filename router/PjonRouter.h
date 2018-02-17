91a4df04996fbfea8674632cd9244f4db2d4a927
Vladislav Gingo Skoumal <skoumal@skoumal.net>
2018-02-17 11:42:17 +0100

    Fixed custom pointer and separated method definition and declaration

--- router/PjonRouter.h
+++ router/PjonRouter.h
@@ -13,27 +13,10 @@ public:
   PjonRouter(PJON<Any> ** buses, uint8_t busCount);
 
   void set_buses(PJON<Any> ** buses, uint8_t busCount);
-
-  // gioblu: I wanna support important bus methods here like begin, recieve, update, ... Make sense?
-  // gioblu: Considering to inherit PjonRouter from PJON class to allow using set of buses the same way as one bus. For example to use Router in Router :-)
-  void begin() {
-    for(uint8_t i = 0; i < _busCount; i++) {
-      _buses[i]->begin();
-    }
-  };
-
-  void set_default() {
-    // set_error(PJON_dummy_error_handler);
-    set_receiver(PJON_dummy_receiver_handler);
-  };
-
-  void set_receiver(PJON_Receiver r) {
-    _receiver = r;
-  };
-
-  void set_custom_pointer(void *pointer) {
-    _custom_pointer = pointer;
-  };
+  void begin();
+  void set_default();
+  void set_receiver(PJON_Receiver r);
+  void set_custom_pointer(void *pointer);
 
   // TODO implement more PJON methods
 
@@ -72,16 +55,42 @@ void PjonRouter<Strategy>::set_buses(PJON<Any> ** buses, uint8_t busCount) {
   for(uint8_t i = 0; i < _busCount; i++) {
     _buses[i]->set_router(true);
     _buses[i]->set_receiver(pjon_router_receiver_handler<Strategy>);
+    _buses[i]->set_custom_pointer(this);
   }
 }
 
 // --- private methods ---
+// gioblu: I wanna support important bus methods here like begin, recieve, update, ... Make sense?
+// gioblu: Considering to inherit PjonRouter from PJON class to allow using set of buses the same way as one bus. For example to use Router in Router :-)
+template<class Strategy>
+void PjonRouter<Strategy>::begin() {
+  for(uint8_t i = 0; i < _busCount; i++) {
+    _buses[i]->begin();
+  }
+};
+
+template<class Strategy>
+void PjonRouter<Strategy>::set_default() {
+  // set_error(PJON_dummy_error_handler);
+  set_receiver(PJON_dummy_receiver_handler);
+};
+
+template<class Strategy>
+void PjonRouter<Strategy>::set_receiver(PJON_Receiver r) {
+  _receiver = r;
+};
+
+template<class Strategy>
+void PjonRouter<Strategy>::set_custom_pointer(void *pointer) {
+  _custom_pointer = pointer;
+};
+
 template<class Strategy>
 void PjonRouter<Strategy>::_on_new_packet_received(uint8_t * payload, uint16_t length, const PJON_Packet_Info &packet_info) {
 
-  bool handled = strategy.handle_packet_before_this_device(payload, length, packet_info);
+  bool handled = strategy.handle_packet_before_this_device(_buses, _busCount, payload, length, packet_info);
 
-  handled = _handle_packet_for_this_device(payload, length, packet_info);
+  handled = _handle_packet_for_this_device(_buses, _busCount, payload, length, packet_info);
 
   if(!handled) {
     strategy.handle_packet(payload, length, packet_info);
@@ -94,12 +103,18 @@ bool PjonRouter<Strategy>::_handle_packet_for_this_device(uint8_t * payload, uin
 
   for(uint8_t i = 0; i < _busCount; i++) {
     if(PJON<Any>::bus_id_equality(_buses[i]->bus_id, packet_info.receiver_bus_id) &&
-      _buses[i]->device_id() == packet_info.receiver_id) {
-        _receiver(
-          payload,
-          length,
-          packet_info
-        );
+        _buses[i]->device_id() == packet_info.receiver_id) {
+
+      PjonRouter<Strategy>* pointer = (PjonRouter<Strategy>*)packet_info.custom_pointer;
+      packet_info.custom_pointer = _custom_pointer;
+
+      _receiver(
+        payload,
+        length,
+        packet_info
+      );
+
+      packet_info.custom_pointer = pointer;
       return true;
     }
 
